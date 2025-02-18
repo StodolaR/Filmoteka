4ae98ec4a436d49be83c5954cc4c450bedab5d3f
StodolaR <sto.dy@seznam.cz>
2025-02-18 23:07:29 +0100

    Pridana trida ViewModelBase

---
+++ Filmoteka/Framework/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmoteka.Framework
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
