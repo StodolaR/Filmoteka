6eca4032e23d4369c68039f8fd049aa83872f052
StodolaR <sto.dy@seznam.cz>
2025-03-10 17:38:37 +0100

    Pridan UserViewModel

---
+++ Filmoteka/ViewModel/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Filmoteka.Framework;
+using Filmoteka.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmoteka.ViewModel
+{
+    public class UserViewModel : ViewModelBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public ObservableCollection<UserMovie> Ratings { get; set; } = new ObservableCollection<UserMovie>();
+    }
+}
