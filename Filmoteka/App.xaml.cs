ea32eb212007ed5b593bc2f9ec42c67552018533
StodolaR <sto.dy@seznam.cz>
2025-03-26 20:44:38 +0100

    Uprava zaloh pro build

--- Filmoteka/App.xaml.cs
+++ Filmoteka/App.xaml.cs
@@ -14,17 +14,42 @@ namespace Filmoteka
         {
             try
             {
-                if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "movies.db")))
+                if (!Directory.Exists(@"Zaloha-ukazka/Posters"))
                 {
-                    File.Copy(Path.Combine(Directory.GetCurrentDirectory(), "Zaloha-prazdna", "movies.db"),
-                        Path.Combine(Directory.GetCurrentDirectory(), "movies.db"));
+                    Directory.CreateDirectory("Zaloha-ukazka/Posters");
+                    File.Copy(@"../../../../Zaloha-ukazka/movies.db", @"Zaloha-ukazka/movies.db", true);
+                    string[] paths = Directory.GetFiles("..\\..\\..\\..\\Zaloha-ukazka\\Posters");
+                    foreach (string path in paths)
+                    {
+                        string target = Path.Combine("Zaloha-ukazka/Posters", Path.GetFileName(path));
+                        File.Copy(path, target, true);
+                    }
+                }
+                if (!Directory.Exists("Zaloha-prazdna"))
+                {
+                    Directory.CreateDirectory("Zaloha-prazdna");
+                    File.Copy(@"../../../../Zaloha-prazdna/movies.db", @"Zaloha-prazdna/movies.db");
+                }
+                if (!File.Exists("movies.db"))
+                {
+                    File.Copy(@"Zaloha-ukazka/movies.db","movies.db");
+                }
+                if (!Directory.Exists("Posters"))
+                {
+                    Directory.CreateDirectory("Posters");
+                    string[] paths = Directory.GetFiles("Zaloha-ukazka\\Posters");
+                    foreach (string path in paths)
+                    {
+                        string target = Path.Combine("Posters", Path.GetFileName(path));
+                        File.Copy(path, target, true);
+                    }
                 }
                 MainWindow mainWindow = new MainWindow();
                 mainWindow.Show();
             }
-            catch
+            catch (Exception ex) 
             {
-                MessageBox.Show("Databáze nenalezena, aplikace bude ukončena");
+                MessageBox.Show($"Databáze nenalezena, aplikace bude ukončena {ex}");
                 Application.Current.Shutdown();
             }
             
