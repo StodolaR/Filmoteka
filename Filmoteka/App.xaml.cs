0d9acbbc25d5af68f27149891e47f9b77a97d340
StodolaR <sto.dy@seznam.cz>
2025-03-26 21:36:46 +0100

    Uprava zaloh pro build

--- Filmoteka/App.xaml.cs
+++ Filmoteka/App.xaml.cs
@@ -14,45 +14,41 @@ namespace Filmoteka
         {
             try
             {
-                if (!Directory.Exists(@"Zaloha-ukazka/Posters"))
-                {
-                    Directory.CreateDirectory("Zaloha-ukazka/Posters");
-                    File.Copy(@"../../../../Zaloha-ukazka/movies.db", @"Zaloha-ukazka/movies.db", true);
-                    string[] paths = Directory.GetFiles("..\\..\\..\\..\\Zaloha-ukazka\\Posters");
-                    foreach (string path in paths)
-                    {
-                        string target = Path.Combine("Zaloha-ukazka/Posters", Path.GetFileName(path));
-                        File.Copy(path, target, true);
-                    }
-                }
-                if (!Directory.Exists("Zaloha-prazdna"))
-                {
-                    Directory.CreateDirectory("Zaloha-prazdna");
-                    File.Copy(@"../../../../Zaloha-prazdna/movies.db", @"Zaloha-prazdna/movies.db");
-                }
                 if (!File.Exists("movies.db"))
                 {
-                    File.Copy(@"Zaloha-ukazka/movies.db","movies.db");
-                }
-                if (!Directory.Exists("Posters"))
-                {
-                    Directory.CreateDirectory("Posters");
-                    string[] paths = Directory.GetFiles("Zaloha-ukazka\\Posters");
-                    foreach (string path in paths)
+                    if (!Directory.Exists(@"Zaloha-ukazka/Posters"))
                     {
-                        string target = Path.Combine("Posters", Path.GetFileName(path));
-                        File.Copy(path, target, true);
+                        Directory.CreateDirectory("Zaloha-ukazka/Posters");
+                        Directory.CreateDirectory("Posters");
+                        File.Copy(@"../../../../Zaloha-ukazka/movies.db", @"Zaloha-ukazka/movies.db", true);
+                        File.Copy(@"../../../../Zaloha-ukazka/movies.db", "movies.db", true);
+                        string[] paths = Directory.GetFiles("..\\..\\..\\..\\Zaloha-ukazka\\Posters");
+                        foreach (string path in paths)
+                        {
+                            string target = Path.Combine("Zaloha-ukazka/Posters", Path.GetFileName(path));
+                            File.Copy(path, target, true);
+                            string target2 = Path.Combine("Posters", Path.GetFileName(path));
+                            File.Copy(path, target2, true);
+                        }
+                        if (!Directory.Exists("Zaloha-prazdna"))
+                        {
+                            Directory.CreateDirectory("Zaloha-prazdna");
+                            File.Copy(@"../../../../Zaloha-prazdna/movies.db", @"Zaloha-prazdna/movies.db");
+                        }
+                    }
+                    else
+                    {
+                        File.Copy(@"Zaloha-prazdna/movies.db", "movies.db");
                     }
                 }
                 MainWindow mainWindow = new MainWindow();
                 mainWindow.Show();
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 MessageBox.Show($"Databáze nenalezena, aplikace bude ukončena {ex}");
                 Application.Current.Shutdown();
             }
-            
         }
     }
 
