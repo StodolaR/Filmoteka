327751dfeeb66f487b2b281f2617e3007b69d979
StodolaR <sto.dy@seznam.cz>
2025-03-26 12:32:08 +0100

    Shutdown pokud neni databaze

--- Filmoteka/App.xaml.cs
+++ Filmoteka/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
 
 namespace Filmoteka
@@ -9,6 +10,25 @@ namespace Filmoteka
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "movies.db")))
+                {
+                    File.Copy(Path.Combine(Directory.GetCurrentDirectory(), "Zaloha-prazdna", "movies.db"),
+                        Path.Combine(Directory.GetCurrentDirectory(), "movies.db"));
+                }
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            catch
+            {
+                MessageBox.Show("Databáze nenalezena, aplikace bude ukončena");
+                Application.Current.Shutdown();
+            }
+            
+        }
     }
 
 }
